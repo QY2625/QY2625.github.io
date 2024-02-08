b25d211abc97a2e54fc3eeff9fffec6e4e86aa45
Recall <101963390+ShallowRecall@users.noreply.github.com>
2024-02-08 21:59:54 +0800

    Site updated: 2024-02-08 21:59:53

---
+++ anzhiyu/random.js
@@ -0,0 +1,1 @@
+var posts=["posts/4a17b156.html","posts/505b9ee7.html","posts/c703b936.html","posts/2353f099.html","posts/85fc7b85.html","posts/62a3a0d6.html","posts/b1609aae.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
